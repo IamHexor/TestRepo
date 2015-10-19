a6fa7fd79662e4d1dd3013aa80cd7eef10a21b1f
Frank <ykhexor@hotmail.com>
2015-10-19 14:40:03 +0200

    tilføjet adamognoah

--- TestProjekt/src/TestClass.java
+++ TestProjekt/src/TestClass.java
@@ -3,5 +3,6 @@ public class TestClass {
 public static void main(String[] args) {
 	System.out.println("Dig med hej");
 	System.out.println("Hej med dig");
+	System.out.println("sker der danskers");
 }
 }
