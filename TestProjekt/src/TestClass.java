5a0cd79401e24207d02d723b00d65f49d0bbb43e
Frank <ykhexor@hotmail.com>
2015-10-19 15:40:44 +0200

    tilføjede syso LOL LEJ

--- TestProjekt/src/TestClass.java
+++ TestProjekt/src/TestClass.java
@@ -4,5 +4,6 @@ public static void main(String[] args) {
 	System.out.println("Dig med hej");
 	System.out.println("Hej med dig");
 	System.out.println("sker der danskers");
+	System.out.println("LOL LEJ");
 }
 }
